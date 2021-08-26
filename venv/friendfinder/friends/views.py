5b0b90c5dcd43ddb63d518b0d7915e2a4071d73c
minnie1804 <minnie180404@gmail.com>
2021-08-26 23:49:24 +0530

    login signup complete

--- venv/friendfinder/friends/views.py
+++ venv/friendfinder/friends/views.py
@@ -9,14 +9,41 @@ from .models import *
 def login(request):
   if request.method == 'POST':
     print(request.POST)
+    email=request.POST["email"]
+    password=request.POST["password"]
+    cursor=connection.cursor()
+    try:
+      cursor.execute("select password from login_detail where email=\'"+email+"\'")
+      data=cursor.fetchall()
+      if password==data[0][0]:
+        return render(request,"home.html")
+      else:
+        return render(request,"error.html")
+    except:
+      return render(request,"error.html")
+      
   return render(request,"login.html")
-
-
+    
 def signup(request):
   if request.method == 'POST':
     print(request.POST)
+    email=request.POST["email"]
+    password=request.POST["password"]
+    cursor=connection.cursor()
+    name=request.POST["name"]
+    try:
+      cursor.execute("select count(*) from login_detail where email=\'"+email+"\'")
+      data=cursor.fetchall()
+      print(data)
+      if data[0][0]==1:
+        return render(request,"error.html")
+      else:
+        print("insert into login_detail values(5,\'"+name+",\'"+email+"\',\'"+password+"\')")
+        cursor.execute("insert into login_detail values(5,\'"+name+"\',\'"+email+"\',\'"+password+"\')")
+        return render(request,"success.html")
+    except:
+      return render(request,"error.html")
   return render(request, "signup.html")
-
 
 def home(request):
   return render(request, "home.html")
